8cfb7d0f62372ebb45e5bfa514e9a18327b24a7e
liurenpeng <liurenpeng@kuaishou.com>
2021-05-23 10:22:13 +0800

    TestByBuilderAndMapper

--- src/test/kotlin/com/agile4j/feed/builder/TestByBuilderAndMapper.kt
+++ src/test/kotlin/com/agile4j/feed/builder/TestByBuilderAndMapper.kt
@@ -4,9 +4,10 @@ import com.agile4j.feed.builder.mock.Article
 import com.agile4j.feed.builder.mock.ArticleView
 import com.agile4j.feed.builder.mock.getArticleByIds
 import com.agile4j.feed.builder.mock.getArticlesByTimeAsc
-import com.agile4j.model.builder.relation.accompanyBy
 import com.agile4j.model.builder.relation.buildBy
 import com.agile4j.model.builder.relation.indexBy
+import com.agile4j.model.builder.relation.invoke
+import com.agile4j.model.builder.relation.targets
 import org.junit.Assert
 import org.junit.Test
 
@@ -16,8 +17,6 @@ import org.junit.Test
  */
 class TestByBuilderAndMapper {
 
-    private val feedBuilder = getBuilderBuilder().build()
-
     private fun getBuilderBuilder(): FeedBuilderBuilder<Long, Long, Article, ArticleView> {
         return FeedBuilderFactory
             .ascLongBuilderEx(Article::class, ArticleView::class, ::getArticlesByTimeAsc)
@@ -26,7 +25,6 @@ class TestByBuilderAndMapper {
             .searchBufferSize{ 3 }
             .searchTimesLimit{ 5 }
             .maxSearchBatchSize{ 100 }
-            //.topNSupplier { listOf(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L, 13L) }
             .topNSupplier { listOf(1L, 2L, 3L, 4L, 5L, 6L) }
             .fixedSupplier(FixedPosition.SECOND) { listOf(6L, 7L, 8L) }
             .indexFilter { it > 0 }
@@ -36,24 +34,23 @@ class TestByBuilderAndMapper {
     }
 
     private fun initModelBuilder() {
-        Article::class indexBy Article::id
-        Article::class buildBy ::getArticleByIds
-
-        ArticleView::class accompanyBy Article::class
+        Article::class {
+            indexBy(Article::id)
+            buildBy(::getArticleByIds)
+            targets(ArticleView::class)
+        }
     }
 
     @Test
     fun testByBuilderAndMapper() {
         initModelBuilder()
 
-        val response = feedBuilder.buildBy("")
+        val response = getBuilderBuilder().build().buildBy("")
         val articleViews: List<ArticleView> = response.list
         val nextCursor: String = response.nextCursor
 
         Assert.assertEquals(10, articleViews.size)
         Assert.assertTrue(nextCursor.startsWith("TAIL;11;11;"))
-        articleViews.forEach{ println(it) }
-        println(nextCursor)
     }
 
     @Test
